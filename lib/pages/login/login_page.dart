e2e4c133da33865945c7287c3ab86e891d126ea5
unknown <okdiantoardi9@gmail.com>
2025-04-09 19:32:21 +0700

    style splash screen dan login

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -7,26 +7,42 @@ import 'package:esalesapp/repositories/user/user_repository.dart';
 import 'package:esalesapp/blocs/authentication/authentication_bloc.dart';
 import 'package:esalesapp/blocs/login/login_bloc.dart';
 import 'package:esalesapp/pages/login/login_form.dart';
+import 'package:esalesapp/widgets//splash_screen.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   final UserRepository userRepository;
 
   const LoginPage({super.key, required this.userRepository});
 
   @override
-  Widget build(BuildContext context) {
-    //debugPrint("Masuk ke Login Page");
+  State<LoginPage> createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
+  bool _showSplash = true;
+
+  // Callback dipanggil ketika animasi splash selesai.
+  void _onSplashFinish() {
+    setState(() {
+      _showSplash = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MobileDesignWidget(
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Login'),
-        ),
-        body: BlocProvider(
+        body: _showSplash
+            ? SplashScreen(
+          imageAsset: 'assets/images/jps-image.png',
+          animationDuration: const Duration(seconds: 3),
+          onFinish: _onSplashFinish,
+        )
+            : BlocProvider(
           create: (context) {
             return LoginBloc(
               authenticationBloc: BlocProvider.of<AuthenticationBloc>(context),
-              userRepository: userRepository,
+              userRepository: widget.userRepository,
             );
           },
           child: const LoginForm(),
